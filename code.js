d6933fb25e43d35037b403baeca45a1ed87d1f30
Thomas Goris <thomas.goris1@student.ucll.be>
2022-10-11 22:08:15 +0200

    werkt mar 400 error

--- code.js
+++ code.js
@@ -3,131 +3,123 @@ const elements = {
   registerPassword: document.querySelector("#register-password"),
   registerButton: document.querySelector("#register-button"),
   loginEmail: document.querySelector("#login-email"),
-  longinPassword: document.querySelector("#longin-password"),
+  longinPassword: document.querySelector("#login-password"),
   loginButton: document.querySelector("#login-button"),
-  gotoLink: document.querySelector("#goto-link"),
+  gotoRegister:document.querySelector("#go-to-register"),
+  gotoLogin: document.querySelector("#go-to-login"),
   newTodoDescription: document.querySelector("#create-todo-description"),
   newTodoButton: document.querySelector("#create-todo-button"),
 };
 
-function showRelevantHTML(id = "login") {
-  for (const section of applicationSections) {
-    const elements = document.querySelector(`#${section}`);
-    if (!originalDisplayOptions.has(section)) {
-      originalDisplayOptions.set(section, elements.style.display);
+const applicationSections = ["loading", "login", "register", "todos"]
+const originalDisplayOptions = new Map();
+
+(async () => {
+ 
+  function showRelevantHTML(id = "loading") {
+    for (const section of applicationSections) {
+      const element = document.querySelector(`#${section}`);
+      if (!originalDisplayOptions.has(section)) {
+        originalDisplayOptions.set(section, element.style.display);
+      }
+      if (section === id) {
+        element.style.display = originalDisplayOptions.get(section);
+      } else {
+        element.style.display = "none";
+      }
     }
-    if (section === id) {
-      elements.style.display = originalDisplayOptions.get(section);
+  }
+
+  elements.gotoRegister.addEventListener("click", () =>showRelevantHTML("register"));
+  elements.gotoLogin.addEventListener("click", () => showRelevantHTML("login"));
+
+  async function registerUser() {
+    const { auth, createUserWithEmailAndPassword } = window.fiba;
+
+    const email = elements.registerEmail.value;
+    const password = elements.registerPassword.value;
+
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      todoApp();
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  async function loginUser() {
+    const { auth, signInWithEmailAndPassword } = window.fiba;
+
+    const email = elements.loginEmail.value;
+    const password = elements.longinPassword.value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      todoApp();
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  async function loadTodoItems() {
+    const { db, collection, getDocs, auth } = window.fiba;
+
+    const { uid } = auth.currentUser;
+    const todoList = document.querySelector('#todo-items-list');
+    const todoResult = await getDocs(collection(db, uid));
+
+
+    todoList.innerHTML = "";
+    todoResult.forEach(todoItem => {
+        const buttonId = `Button"${todoItem.id}`
+        todoList.innerHTML += `<li>${todoItem.data().description}</li><button onclick="deleteTodoItem('${todoItem.id}')" id='${buttonId}'>X</button>`
+
+    });
+}
+
+  async function todoApp() {
+    const { auth } = window.fiba;
+    elements.registerButton.addEventListener("click", registerUser);
+    elements.loginButton.addEventListener("click", loginUser);
+    elements.newTodoButton.addEventListener("click", addTodoItem);
+
+    if (auth.currentUser) {
+      showRelevantHTML("todos");
+      await loadTodoItems();
     } else {
-      elements.style.display = "none";
+      showRelevantHTML("login");
     }
   }
-}
 
-elements.gotoRegister.addEventListener("click", () =>
-  showRelevantHTML("register")
-);
-elements.gotoLogin.addEventListener("click", () => showRelevantHTML("login"));
-
-async function registerUser() {
-  const { auth, createUserWhitEmaiAndlPassword } = window.fiba;
-
-  const email = elements.registerEmail.value;
-  const password = elements.registerPassword.value;
-
-  try {
-    await createUserWhitEmaiAndlPassword(auth, email, password);
-    todoApp();
-  } catch (error) {
-    alert(error.message);
+  function waitForFirebaseAndStart() {
+    if (!window.fiba?.isReady()) {
+      console.log("Waiting for Firebase");
+      setTimeout(waitForFirebaseAndStart, 500);
+    } else {
+      console.log("Firebase is ready");
+      todoApp();
+    }
   }
-}
 
-async function loginUser() {
-  const { auth, createUserWhitEmaiAndlPassword } = window.fiba;
+  waitForFirebaseAndStart();
 
-  const email = elements.loginEmail.value;
-  const password = elements.loginPassword.value;
+  async function addTodoItem() {
+    try {
+      const { db, setDoc, doc, auth } = window.fiba;
+      const userId = auth.currentUser.uid;
+      const newTodoItemId = crypto.randomUUID();
 
-  try {
-    await createUserWhitEmaiAndlPassword(auth, email, password);
-    todoApp();
-  } catch (error) {
-    alert(error.message);
+      const newTodoItem = {
+        description: elements.newTodoDescription.value,
+        createTimestamp: Date.now(),
+      };
+
+      await setDoc(doc(db, userId, newTodoItemId), newTodoItem);
+      loadTodoItems();
+    } catch (error) {
+      alert(error.message);
+    }
   }
-}
 
-async function loadTodoItems() {
-  const { db, collection, getDocs, auth } = window.fiba;
-
-  const { uid } = auth.currentUser;
-  const todoList = document.querySelector("#todo-items-list");
-  const todoResult = await getDocs(collection(db, uid));
-
-  todoList.innerHTML = "";
-  todoResult.forEach((todoItem) => {
-    todoList.innerHTML += `<li>${todoItem.data().description}</li>`;
-  });
-}
-
-async function todoApp() {
-  const { auth } = window.fiba;
-  elements.registerButton.addEventListener("click", registerUser);
-  elements.loginButton.addEventListener("click", loginUser);
-  elements.newTodoButton.addEventListener("click", addTodoItem);
-
-  if (auth.currentUser) {
-    showRelevantHTML("todos");
-    await loadTodoItems();
-  } else {
-    showRelevantHTML("login");
-  }
-}
-
-function waitForFirebaseAndStart() {
-  if (!window.fiba?.isReady()) {
-    console.log("Waiting for Firebase");
-    setTimeout(waitForFirebaseAndStart, 500);
-  } else {
-    console.log("Firebase is ready");
-    todoApp();
-  }
-}
-
-waitForFirebaseAndStart();
-
-async function addTodoItem() {
-  try {
-    const { db, setDoc, doc, auth } = window.fiba;
-    const userId = auth.currentUser.uid;
-    const newTodoItemId = crypto.randomUUID;
-
-    const newTodoItem = {
-      description: elementsLocated.newTodoDescription.value,
-      createTimestamp: Date.now(),
-    };
-
-    await setDoc(doc(db, userId, newTodoItem), newTodoItem);
-    loadTodoItems();
-  } catch (error) {
-    alert(error.message);
-  }
-}
-
-async function todoApp() {
-  const { collection, getDocs, db } = window.fiba;
-  await loadTodoItems(getDocs, db, collection);
-}
-function waitForFirebaseAndStart() {
-  if (!window.fiba?.isReady()) {
-    console.log("Waiting for firebase");
-    setTimeout(waitForFirebaseAndStart, 500);
-  } else {
-    console.log("firebase ready!");
-    todoApp();
-  }
-}
-
-waitForFirebaseAndStart();
-
-await loadTodoItems();
+  await loadTodoItems();
+})();
